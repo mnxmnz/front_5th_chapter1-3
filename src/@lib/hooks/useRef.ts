c43a4d8438f686fc5e1b680f3442cf28ed422e4b
mnxmnz <minzidev@gmail.com>
2025-04-06 20:17:59 +0900

    feat: useRef 훅 구현 - 초기값을 유지하고 리렌더링 시 동일 객체 반환

--- src/@lib/hooks/useRef.ts
+++ src/@lib/hooks/useRef.ts
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 export function useRef<T>(initialValue: T): { current: T } {
-  // React의 useState를 이용해서 만들어보세요.
-  return { current: initialValue };
+  // 컴포넌트의 첫 렌더링에서만 초깃값 사용
+  // 컴포넌트의 생명주기 동안 같은 객체 유지
+  // 리렌더링시에는 이전에 생성한 객체 반환
+  const [ref] = useState({ current: initialValue });
+
+  return ref;
 }
