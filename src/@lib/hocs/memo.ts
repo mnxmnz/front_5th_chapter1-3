cab5f43762a8a1a7c2abd1741ce35ff62ea5f9de
mnxmnz <minzidev@gmail.com>
2025-04-10 00:13:43 +0900

    feat: memo 함수에 컴포넌트 유효성 검사 및 props 변경 시 메모이제이션 기능 추가

--- src/@lib/hocs/memo.ts
+++ src/@lib/hocs/memo.ts
@@ -1,10 +1,31 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+import { ComponentType, createElement, isValidElement } from "react";
+
 import { shallowEquals } from "../equalities";
-import { ComponentType } from "react";
+import { useRef } from "../hooks";
 
 export function memo<P extends object>(
   Component: ComponentType<P>,
   _equals = shallowEquals
 ) {
-  return Component;
+  // 컴포넌트 유효성 검사
+  if (!isValidElement(createElement(Component))) {
+    throw new Error("Invalid component passed to memo");
+  }
+
+  return function Memoized(props: P) {
+    const prevProps = useRef(props);
+    const memoizedComponent = useRef<JSX.Element>(
+      createElement(Component, props)
+    );
+
+    const isPropsChanged = !_equals(prevProps.current, props);
+
+    // props 가 변경된 경우에만 컴포넌트를 새로 생성
+    if (isPropsChanged) {
+      prevProps.current = props;
+      memoizedComponent.current = createElement(Component, props);
+    }
+
+    return memoizedComponent.current;
+  };
 }
